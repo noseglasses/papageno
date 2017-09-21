e3d9b9d788eeb2f7e3c09c87640b1f59f415534f
Florian Fleissner <fleissner@itm.uni-stuttgart.de>
2017-09-21 11:41:21 +0200

    Fixed some problems with error reporting during simavr based tests

--- testing/char_strings/TestAndCheck.script.cmake
+++ testing/char_strings/TestAndCheck.script.cmake
@@ -1,12 +1,14 @@
-
 execute_process(
    COMMAND ${CMD}
    OUTPUT_VARIABLE output
    ERROR_VARIABLE error
+   RESULT_VARIABLE result
 )
-if(("${output}" MATCHES "__PAPAGENO_TEST_FAILED__")
-   OR ("${error}" MATCHES "__PAPAGENO_TEST_FAILED__"))
 
+if(   ("${output}" MATCHES "__PAPAGENO_TEST_FAILED__")
+   OR ("${error}" MATCHES "__PAPAGENO_TEST_FAILED__")
+   OR (NOT "${result}" EQUAL 0)
+)
    message("Error running ${CMD}")
    message("output: ${output}")
    message("error: ${error}")
