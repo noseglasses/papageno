0b54b84a6cf229ef1ecf53447f67832411cd94ea
noseglasses <shinynoseglasses@gmail.com>
2018-01-08 17:38:34 +0100

    Fixed author name in file headers

--- testing/char_strings/test_clusters.c
+++ testing/char_strings/test_clusters.c
@@ -1,4 +1,4 @@
-/* Copyright 2017 Florian Fleissner
+/* Copyright 2017 noseglasses <shinynoseglasses@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
